5d6411c67e52fca265cda7e6c23ee72b07ad2636
Agbemazi Dickson <63107056+agbemazidickson@users.noreply.github.com>
2021-03-11 22:19:42 +0000

    Closes:#2
    Refactored code and changed name of variables to descriptive ones
    Deleted files generated by clean_ticky.py and ticky.py scripts

--- ticky/clean_ticky.py
+++ ticky/clean_ticky.py
@@ -1,90 +1,81 @@
 #! python
 
-import operator
-import csv
-import re
+import operator, csv, re
 
-
-
-#per_usage_stats = {}
 errors = {}
 user_info_count = {}
 error_user = {}
+usage_stats = {}
 
 
-def search():
+def search_errors_info():
+
     with open('syslog.log') as file:
         for line in file:
             if ' ERROR: ' in line:
+
                 find_error = re.search(r"ticky: ERROR: ([\w ]*) .*\(([\w ]*)\)", line)
-                answer = find_error.group(1)
-                if answer not in errors:
-                    errors[answer] = 1
+                error_description = find_error.group(1)
+
+                if error_description not in errors:
+                    errors[error_description] = 1
                 else:
-                    errors[answer] += 1
+                    errors[error_description] += 1
+
                 error_count = find_error.group(2)
+
                 if error_count not in error_user:
                     error_user[error_count] = 1
                 else:
                     error_user[error_count] += 1
-                
-search()
-hi = dict(sorted(errors.items(), key=operator.itemgetter(1), reverse=True))
 
-
-
-def search_info_count():
-    with open('syslog.log') as infos:
-        for line in infos:
-            if ' INFO: ' in line:
+            elif ' INFO: ' in line:
                 find_user = re.search(r'ticky: INFO: .*\(([\w ]*)\)', line)
                 res = find_user.group(1)
                 if res not in user_info_count:
                     user_info_count[res] = 1
                 else:
-                    user_info_count[res] += 1
+                    user_info_count[res] += 1               
+    file.close()
 
 
-combined = {}
-def main():
-    search_info_count()
-    search()
-    total = [user_info_count, error_user]
 
-    for k in user_info_count.keys():
-        combined[k] = tuple(combined[k] for combined in total)
+def combine_data():
 
+    list_of_dicts = [user_info_count, error_user]
 
-main()
-
+    for user in user_info_count.keys():
+        usage_stats[user] = tuple(usage_stats[user] for usage_stats in list_of_dicts)
 
 
 def write_error_csv():
-    with open("errors.csv", 'w', newline="") as error:
-        fieldnames = ["Errors", "Count"]
-        writer = csv.DictWriter(error, fieldnames=fieldnames)
+    sorted_dict = dict(sorted(errors.items(), key=operator.itemgetter(1), reverse=True))
 
+    with open("errors.csv", 'w', newline="") as error:
+        field_names = ["Errors", "Count"]
+        writer = csv.DictWriter(error, fieldnames=field_names)
         writer.writeheader()
 
-        for key, value in hi.items():
-            h = {"Errors": key, "Count": value}
-            writer.writerow(h)
-
-
-write_error_csv()
+        for key, value in sorted_dict.items():
+            row_data = {"Errors": key, "Count": value}
+            writer.writerow(row_data)
 
 
 def write_usage_stats():
-    with open("stats.csv", "w", newline="") as stats:
-        fieldnames = ["USER", "INFO", "ERROR"]
 
-        writer = csv.DictWriter(stats, fieldnames=fieldnames)
+    with open("stats.csv", "w", newline="") as stats:
+        field_names = ["USER", "INFO", "ERROR"]
+        writer = csv.DictWriter(stats, fieldnames=field_names)
         writer.writeheader()
 
-
-        for key, value in combined.items():
+        for key, value in usage_stats.items():
             info, error = value
-            e = {"USER": key, "INFO": info, "ERROR": error}
-            writer.writerow(e)
+            csv_row_data = {"USER": key, "INFO": info, "ERROR": error}
+            writer.writerow(csv_row_data
 
-write_usage_stats()
+
+if "__name__" == "__main__":
+    search_errors_info()
+    combine_data()
+    write_usage_stats()
+    write_error_csv()
--- ticky/ticky.py
+++ ticky/ticky.py
@@ -1,18 +1,20 @@
 #! python
+#! /usr/bin /env python3
 
 import random
 import datetime
 
 
-errors = ["INFO", 'ERROR']
-usernames = ['james', 'mcintosh', 'hayle', 'fsusan', 'greg', 'hayden', 'fentuo', 'sysadmin', 'fblay']
-description = ['Created ticket', 'Failed to connect to DB', 'Service timeout', 'Bad input']
-date = datetime.date.today()
-# grap errors from syslog
+msg_type = ["INFO", 'ERROR']
 
+usernames = ['james', 'mcintosh', 'hayle', 'fsusan', 'greg', 'hayden', 'fentuo', 'sysadmin', 'fblay']
+descriptions = ['Created ticket', 'Failed to connect to DB', 'Service timeout', 'Bad input']
+date = datetime.date.today()
+
+# Generate pseudo syslog.log to use to test clean_ticky.log
 with open("syslog.log", 'w') as logfile:
     for user in usernames:
-        for error in errors:
-            for info in description:
+        for msg in msg_type:
+            for description in descriptions:
                 numbers = random.randint(1000, 4000)
-                logfile.write(f"{date} Ubuntu debian server ticky: {error}: {info} [#{numbers}] ({user})\n")
+                logfile.write(f"{date} Ubuntu debian server ticky: {msg}: {description} [#{numbers}] ({user})\n")
