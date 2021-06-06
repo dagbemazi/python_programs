f5a2911a82efb2f81696b28403c18a76e9f5a562
Agbemazi Dickson <63107056+agbemazidickson@users.noreply.github.com>
2021-06-06 13:02:22 +0000

    Adding more entry points and cache files to ignore

--- cli_todolist/setup.py
+++ cli_todolist/setup.py
@@ -4,10 +4,12 @@ from setuptools import setup
 setup(
         name="todo_list",
         version="0.1",
-        install_requires=["Click", ],
+        author="Agbemazi Dickson",
+        install_requires=["Click", "prettytable", ],
         py_modules=["todo_list"],
         entry_points="""
 	[console_scripts]
+        todo-list=todo_list:cli
 	add=todo_list:add
 	show=todo_list:show
 	delete=todo_list:delete
