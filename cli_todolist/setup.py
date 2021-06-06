f7dc1dd36240c0b031bfcc89642bb5f3f9d59686
Agbemazi Dickson <63107056+agbemazidickson@users.noreply.github.com>
2021-06-06 12:45:51 +0000

    Updating entry points in setup file

--- cli_todolist/setup.py
+++ cli_todolist/setup.py
@@ -8,6 +8,9 @@ setup(
         py_modules=["todo_list"],
         entry_points="""
 	[console_scripts]
-	add=todo_list:prompt_task
+	add=todo_list:add
+	show=todo_list:show
+	delete=todo_list:delete
+	edit=todo_list:edit
 	"""
 )
